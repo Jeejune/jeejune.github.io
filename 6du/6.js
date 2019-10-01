2aa6ba6af5e242a3186afbddc467f9b78c324f0f
Tq-Lj <6du@protonmail.com>
2019-10-01 09:45:36 +0800

    .

--- 6du/6.js
+++ 6du/6.js
@@ -1,19 +1,19 @@
 (async () => {
   const
-    get=async (url)=>await (await fetch()).text(),
     doc=document,
     tag=(name,attr)=>{
       var e = doc.createElement(name)
       Object.assign(e,attr)
       doc.head.appendChild(e)
     },
-    script=(src)=>tag('script',{src});
+    script=(src)=>tag('script',{src})
+    ;
 
   // 请替换为自己的腾讯统计
   script('//tajs.qq.com/stats?sId=66475325')
 
   //加载页面
-  var npm = C.cdn.npm+"/", t=await get(npm+"6").split("\n");
+  var npm = C.cdn.npm+"/", t=await (await fetch(npm+6)).text().split("\n");
   for(let i=0;i<4;++i){
     for (let s of t[i].split(' ')){
       s = npm+s+'.'
